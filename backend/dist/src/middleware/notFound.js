60388a610db16db08c813922b03a73b5b91927d3
SNEHITHKALLUMMAL <snehithkallummal999@gmail.com>
2026-08-05 08:38:16 +0530

    Delete backend directory

--- backend/dist/src/middleware/notFound.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFound = notFound;
-const ApiError_1 = require("../utils/ApiError");
-function notFound(req, _res, next) {
-    next(ApiError_1.ApiError.notFound(`Route not found: ${req.method} ${req.originalUrl}`));
-}
-//# sourceMappingURL=notFound.js.map
